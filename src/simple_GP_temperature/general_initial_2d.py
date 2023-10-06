98df3e4ba7164b130d1235b5e13ae21175e829fa
ChenglinW <chenglin.weng@kcl.ac.uk>
2023-10-06 20:45:59 +0800

    Run all experiemts for the ackley and schaffer functions

---
+++ src/simple_GP_temperature/general_initial_2d.py
@@ -0,0 +1,12 @@
+import random as rd
+class GeneralInitial2D:
+    def __init__(self, input_matrix):
+        self.input_matrix = input_matrix
+        self.backup_matrix = input_matrix
+
+
+    def generate_initial_samples(self): 
+        if self.input_matrix <= 0:
+            self.input_matrix = self.backup_matrix
+        returned_sample = self.input_matrix.pop(0)
+        return returned_sample
